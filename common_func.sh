8fefcdeace011c0152f4f2a35fb8b2ad2b3f348f
Marvin Liu <marvin.liu@ubnt.com>
2017-02-13 18:12:17 +0800

    add script including common used function

---
+++ common_func.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#
+#  add following line to use the common_func.sh
+#  ## SCRIPT_DIR=$(dirname ${BASH_SOURCE[0]})
+#  ## [ ! -e $SCRIPT_DIR/common_func.sh ] && echo "$SCRIPT_DIR/common_func.sh does not exist" && exit
+#  ## source $SCRIPT_DIR/common_func.sh
+#
+#
+
+NC='\e[0m'               # reset to default state
+RED='\e[31m'
+GREEN='\e[32m'
+YELLOW='\e[33m'
+
+BOLD='\033[1m'
+#dbg_enabled=0           # if dbg msg is desired, set to '1'
+
+function err_msg(){
+  echo -e "\n ${RED} "${1}" ${NC} \n" && exit 1
+}
+
+function dbg_msg(){
+  [ ! -z $dbg_enabled ] && [ $dbg_enabled -eq 1 ] && echo -e "${YELLOW} ${1} ${NC}"
+}
+
+dbg_msg "source common_func.sh"
